780c032a17ca03b52f71ffb314a55a801560b3ab
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-26 17:37:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,10 +2,13 @@ def sort_array_asc(array)
 array.sort do |a,b|
   a <=> b
 end 
+
 def sort_array_desc(array)
   array.sort do |a,b|
     b <=> a 
   end 
+end
+  
   def sort_array_char_count(array)
   array.sort do |a, b|
     a.length <=> b.length
@@ -21,6 +24,7 @@ def kesha_maker(array)
   new_array
 end
 
+
 def add_s(array)
    return_array = []
       array.each_with_index do |word,index|
@@ -29,4 +33,3 @@ def add_s(array)
         end
         return_array << word 
   end
-end 
